6ce8f786cfd231717e3431bf5610288cce2dacd3
neko49 <noudehouen-regis-jaures.fassinou@estiam.com>
2022-12-22 17:52:39 +0100

    Update flight_map.py

--- flight_map.py
+++ flight_map.py
@@ -1,17 +1,17 @@
 import csv
+from typing import List
 
 from flight import Flight
 from airport import Airport
 
 class FlightMap :
     def __init__(self):
-        self.aeroports = [Airport]
-        self.flight = [Flight]
+        self.aeroports = []
+        self.flight = []
     
-    def import_airports(self, csv_file : str ):
-        import csv
-        with open(csv_file, newline='') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=' ')
+    def import_airports(self, csv_file : str ) -> None:
+        with open(csv_file, 'r') as f:
+            csv_reader = csv.reader(f)
             next(csv_reader)
             for row in csv_reader:
                 name, code, lat, long = row 
@@ -28,15 +28,26 @@ class FlightMap :
                 vol = Flight(src_code, dst_code, float(duration))
                 self.flight.append(vol)
                 
-    def airports(self):
+    def airports(self) -> List[Airport] :
         return self.aeroports
     
     def flights(self):
         return self.flight
     
+    def airport_find(self, airport_code: str) -> Airport:
+        for airport in self.aeroports:
+            if airport.code == airport_code:
+                return airport
+            return None
     
-
+    def flight_exist(self, src_airport_code: str, dst_airport_code: str) -> Flight:
+        for flight in self.flight:
+            if flight.src_code == src_airport_code and flight.dst_code == dst_airport_code:
+                return True
+            return False
     
+    
+ 
     
         
         
